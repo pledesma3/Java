1233a075040356355137c41cc5e524ff115686e4
pledesma3 <pedroledesma4@gmail.com>
2017-09-19 14:11:00 -0700

    This is a simple stack example

--- stackExample1.java
+++ stackExample1.java
@@ -3,47 +3,47 @@
 //Professor Chatterjee
 //This program is the stack example from lecture 
 class StackMethods{
-	int[] stack;
-	int top;
-	int size;
+	int[] stack;		//array for the stack
+	int top;			//variable for the top element in the stack
+	int size;			//variable for size of the stack
 	
 	public StackMethods(int arraySize){
-		size = arraySize;
+		size = arraySize;		//initialize the stack size
 		stack = new int[size];
 		top = -1;
 		
 	}
 	
 	public void push(int value){
-		if(top == size -1)
+		if(top == size -1)	//print this if you are trying to push more elements than the given size
 			System.out.println("Stack Overflow ");
 		else
 		{
-			top = top + 1;
-			stack[top] = value;
+			top = top + 1;			//place the pushed value onto the stack array
+			stack[top] = value;		//create a new top value when that value is pushed on stack
 		}
 	}
 	
-	public int pop(){
+	public int pop(){		//method to pop values out from stack LIFO
 		
-		int returnValue = -1;
-		if(!isEmpty() )
+		int returnValue = -1;		//if stack is empty return -1
+		if(!isEmpty() )				//if stack is not empty
 		{
-			returnValue = stack[top];
+			returnValue = stack[top];	//pop out the last value pushed on stack
 			top--;
 		}
-		else
+		else						//if empty then print stack underflow
 			System.out.println("Stack Underflow");
 		
 		return returnValue;
 	}
 	
-	public boolean isEmpty(){
+	public boolean isEmpty(){	//if the stack is empty, return -1
 		
 		return top == -1;
 	}
 	
-	public void display(){
+	public void display(){		//method to display the stack
 		System.out.println("The content of the stack is: ");
 		for(int i = 0; i <= top; i++)
 			System.out.print(stack[i] + " | ");
@@ -56,17 +56,18 @@ class StackMethods{
 }
 public class stackExample1 {
 	public static void main(String[] args){
-		StackMethods newStack = new StackMethods(5);
-		newStack.display();
-		newStack.push(10);
-		newStack.push(20);
-		newStack.display();
-		newStack.push(100);
-		newStack.push(90);
-		newStack.display();
-		newStack.pop();
-		newStack.display();
-		System.out.println(newStack.peek());
+		StackMethods newStack = new StackMethods(5); //the stack size will be 5
+		newStack.display();		//Display the empty stack
+		newStack.push(10);		//push value 10 onto stack
+		newStack.push(20);		//push value 20 onto stack
+		newStack.display();		//display what is currently on the stack
+		newStack.push(100);		//push value 100 onto the stack
+		newStack.push(90);		//push value 90 onto the stack
+		newStack.push(80);		//push value 80 onto the stack
+		newStack.display();		//display what is currently on the stack	
+		newStack.pop();			//pop out the last number pushed onto the stack
+		newStack.display();		//display the stack
+		System.out.println(newStack.peek()); //print out the value that is at the peek of the stack
 	}
 
 }
